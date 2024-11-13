9dd3fe756771fc447141547b8a2b3bed4768ccfb
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2024-11-13 21:56:08 +0100

    feat: Create handler for inputs
    Encapsulation of the inputs provided to the action in order to validate
    them.
    Encapsulation and single responsability

---
+++ src/args.ts
@@ -0,0 +1,36 @@
+import { getInput } from '@actions/core'
+
+export class Arguments {
+  forkedRepository: string
+  pluginArtifactName: string
+  pluginID: string
+  filePath: string
+  jmeterPluginsRepository: string
+  githubToken: string
+  constructor() {
+    this.forkedRepository = this.getValidatedInput('FORKED-REPOSITORY')
+    this.pluginArtifactName = this.getValidatedInput('PLUGIN-ARTIFACT-NAME')
+    this.pluginID = this.getValidatedInput('PLUGIN-ID')
+    this.filePath = this.getValidatedInput('REPO-FILE-PATH')
+    this.jmeterPluginsRepository = this.getValidatedInput(
+      'JMETER-PLUGINS-REPOSITORY'
+    )
+    this.githubToken = this.getGithubToken()
+  }
+
+  private getValidatedInput(input: string): string {
+    const value: string = getInput(input)
+    if (value == '') {
+      throw Error(`${input} is not set in the environment or empty`)
+    }
+    return value
+  }
+
+  private getGithubToken(): string {
+    const githubToken: string | undefined = process.env.GITHUB_TOKEN
+    if (githubToken) {
+      return githubToken
+    }
+    throw Error('GITHUB_TOKEN is not available in the environment')
+  }
+}
