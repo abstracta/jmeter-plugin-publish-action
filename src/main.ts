9c307a5b73a7983567a3710e165a37a97bf81f47
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2024-11-18 22:47:35 +0100

    feat: Add apply release changes
    Actually write the release created into disk. Serialize the release into
    its corresponding file.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { setFailed } from '@actions/core'
 import { Arguments } from './args.ts'
 import { GitService } from './git.ts'
 import { GithubService } from './github.ts'
+import { ReleaseBuilder } from './release-builder.ts'
+import type { Plugin, PluginVersion } from './jmeter-plugins.d.ts'
+import { writeFileSync } from 'fs'
 
 const REPOSITORY_NAME = 'jmeter-plugins'
 
@@ -13,13 +16,34 @@ export async function run(): Promise<void> {
   try {
     const args: Arguments = new Arguments()
     const gitHandler: GitService = new GitService(args)
-
     await gitHandler.init(REPOSITORY_NAME)
-    const githubService: GithubService = await GithubService.getInstance(
-      args.githubToken
+    const githubService: GithubService = await GithubService.getInstance(args.githubToken)
+    const releaseBuilder: ReleaseBuilder = new ReleaseBuilder(args, githubService.getAssets())
+    const release: PluginVersion = await releaseBuilder.build()
+    const version: string = githubService.getReleaseVersion()
+    await applyRelease(
+      await releaseBuilder.findFilePluginRepository(),
+      release,
+      args.pluginID,
+      version
     )
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) setFailed(error.message)
+  }
+
+  async function applyRelease(
+    releaseFile: string,
+    release: PluginVersion,
+    pluginID: string,
+    releaseVersion: string
+  ): Promise<void> {
+    const plugins: Plugin[] = await ReleaseBuilder.readPluginsFromFile(releaseFile)
+    const plugin: Plugin | undefined = plugins.find(p => p.id === pluginID)
+    if (plugin) {
+      plugin.versions[releaseVersion] = release
+      writeFileSync(releaseFile, JSON.stringify(plugins, null, 2), 'utf-8')
+      return
+    }
+    throw Error(`The plugin id:"${pluginID}" was not found in ${releaseFile}`)
   }
 }
